055f8ee4d63c7466ca431dbfd1fafcededa8dce9
Tegar Wijaya Kusuma <64220909+tgr-wjya@users.noreply.github.com>
2026-03-19 05:35:47 +0700

    add biome configuration file, update biome version in lock files, and refactor app.ts for consistency

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,14 @@
  */
 
 import { swagger } from "@elysiajs/swagger";
-import { Elysia, t } from "elysia";
+import { Elysia } from "elysia";
 import { buildKaomojiRoutes } from "./routes/kaomoji";
 import { kaomojiService } from "./service/kaomoji.service";
 
 // TODO: Add API Group
 
 export function kaomojiApiApp(group = new kaomojiService()) {
-	const app = new Elysia().use(buildKaomojiRoutes(group)).use(swagger());
+  const app = new Elysia().use(buildKaomojiRoutes(group)).use(swagger());
 
-	return app;
+  return app;
 }
